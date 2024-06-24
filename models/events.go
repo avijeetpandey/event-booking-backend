61891ca2c20d945209c3dcc9515d59abc219ea3d
avijeet <avijeetpandey25@gmail.com>
2024-06-24 23:42:52 +0530

    Add: SQL connection and updates api

--- models/events.go
+++ models/events.go
@@ -1,10 +1,14 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/avijeetpandey/event-booking/db"
+)
 
 // Event model
 type Event struct {
-	ID          int
+	ID          int64
 	Name        string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -12,12 +16,53 @@ type Event struct {
 	UserID      int
 }
 
-var events = []Event{}
+func (e Event) Save() error {
+	query := `
+		INSERT INTO events(name, description, location, dateTime, user_id) VALUES (?, ?, ?, ?, ?)
+	`
 
-func (e Event) Save() {
-	events = append(events, e)
+	preparedStatement, err := db.GlobalDB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer preparedStatement.Close()
+
+	result, err := preparedStatement.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	e.ID = id
+
+	return err
 }
 
-func GetAllEvents() []Event {
-	return events
+func GetAllEvents() ([]Event, error) {
+	query := `SELECT * from events`
+	rows, err := db.GlobalDB.Query(query)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var events []Event
+
+	for rows.Next() {
+		var event Event
+		err := rows.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+		if err != nil {
+			return nil, err
+		}
+
+		events = append(events, event)
+	}
+
+	return events, nil
 }
