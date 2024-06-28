23eca5329053f2661376b3fac32b1b391ce5f0a4
avijeet <avijeetpandey25@gmail.com>
2024-06-29 01:18:07 +0530

    Add: functionality to generate auth token

--- utils/jwt.go
+++ utils/jwt.go
@@ -6,12 +6,15 @@ import (
 	"github.com/golang-jwt/jwt/v5"
 )
 
+const secretKey = "supersecret"
 
 // generating JSON web token
-func GenerateToken() {
-	jwt.NewWithClaims(jwt.SigningMethodES256, jwt.MapClaims{
-		"email":  "",
-		"userId": "",
+func GenerateToken(email string, userId int64) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": userId,
 		"exp":    time.Now().Add(time.Hour * 2).Unix(),
 	})
+
+	return token.SignedString([]byte(secretKey))
 }
